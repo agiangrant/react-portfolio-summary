07609b403815e6d97a4f917512c4c172ec65100f
Andrew Giangrant <andrew.giangrant@outlook.com>
2021-05-13 02:24:46 -0700

    use hooks and handle loading on portfolio summary

--- webapp/src/components/portfolio/PortfolioSummary.js
+++ webapp/src/components/portfolio/PortfolioSummary.js
@@ -1,13 +1,43 @@
+import LoadingOverlay from 'components/common/LoadingOverlay';
+import useGetPortfolio from 'hooks/useGetPortfolio';
+import useGetSecurityPrices from 'hooks/useGetSecurityPrices';
 import React from 'react';
-import './PortfolioSummary.css';
+import styled from 'styled-components';
+import PortfolioCard from './PortfolioCard';
+
+const SummaryContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding: 1rem;
+`;
 
 function PortfolioSummary() {
+  const { loading: portfolioLoading, data: portfolioData } = useGetPortfolio();
+  const {
+    loading: pricesLoading, data: pricesData,
+  } = useGetSecurityPrices(portfolioData?.map((d) => d.id) || []);
 
-    return (
-        <div className="portfolio-summary-container">
-            Start building here!
-        </div>
-    );
+  const card = portfolioData && pricesData ? <SummaryContainer>
+      <PortfolioCard title="Portfolio Value" portfolioData={portfolioData} pricesData={pricesData} />
+    </SummaryContainer> : null;
+
+  if (portfolioLoading || pricesLoading) {
+    return <React.Fragment>
+        <LoadingOverlay />
+        {card}
+      </React.Fragment>;
+  }
+
+  if (portfolioData === null) {
+    return <SummaryContainer>We could not find your portfolio!</SummaryContainer>;
+  }
+
+  if (pricesData === null) {
+    return <SummaryContainer>We could not find pricing for stocks</SummaryContainer>;
+  }
+
+  return card;
 }
 
 export default PortfolioSummary;
