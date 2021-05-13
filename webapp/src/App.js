7230717092cb0a9c54ec20dbd7b0d4b93762c814
Andrew Giangrant <andrew.giangrant@outlook.com>
2021-05-13 02:25:44 -0700

    add theme provider for fluent UI and global styles

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import NavBar from "components/nav/NavBar";
-import PortfolioSummary from "components/portfolio/PortfolioSummary";
+import { useSelector } from 'react-redux';
+import NavBar from 'components/nav/NavBar';
+import PortfolioSummary from 'components/portfolio/PortfolioSummary';
+import styled, { createGlobalStyle } from 'styled-components';
+import { ThemeProvider } from '@fluentui/react';
 
-import './App.css';
+// used to style body and html with the current theme
+const StyledGlobal = createGlobalStyle`
+  html, body, #root {
+    height: 100%;
+    ${({ theme }) => `
+        background-color: ${theme.colors.background};
+        color: ${theme.colors.defaultFont};
+      `}
+  }
+`;
+
+const StyledApp = styled.div`
+  height: 100%;
+`;
 
 function App() {
+  const theme = useSelector((state) => state.theme);
+
   return (
-    <div className="App">
-      <NavBar/>
-      <PortfolioSummary/>
-    </div>
+    <React.Fragment>
+      <StyledGlobal theme={theme}></StyledGlobal>
+      <ThemeProvider theme={theme}>
+        <StyledApp>
+          <NavBar/>
+          <PortfolioSummary/>
+        </StyledApp>
+      </ThemeProvider>
+    </React.Fragment>
   );
 }
 
